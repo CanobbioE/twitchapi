c093006494227e99920952128a15ef325e60dd15
CanobbioE <canobbio.e@gmail.com>
2018-03-09 14:41:59 +0100

    Refactoring

--- types.go
+++ types.go
@@ -34,15 +34,6 @@ type UserFollows struct {
 	FollowedAt string `json:"followed_at"`
 }
 
-// FollowQueryParameters represents the optional query string's parameters used for API calls to the "follow" endpoint.
-type FollowQueryParameters struct {
-	After  string `after`
-	Before string `before`
-	First  int    `first`
-	FromID string `from_id`
-	ToID   string `to_id`
-}
-
 // ------------- Clips -------------
 
 // clipData represents an array of Clip
@@ -138,8 +129,8 @@ type Hero struct {
 	Name  string `json:"name"`
 }
 
-// metas represents an array of StreamMetadata
-type metas struct {
+// metaData represents an array of StreamMetadata
+type metaData struct {
 	Data       []StreamMetadata `json:"data"`
 	Pagination Cursor           `json:"pagination"`
 }
@@ -171,19 +162,6 @@ type Cursor struct {
 	Cursor string `json:"cursor"`
 }
 
-// StreamQueryParameters represents the optional query string parameters used for API calls to the "stream" endpoint.
-type StreamQueryParameters struct {
-	After      string   `after`
-	Before     string   `before`
-	ComunityID []string `comunity_id`
-	First      int      `first`
-	GameID     string   `game_id`
-	Language   string   `language`
-	Type       string   `type`
-	UserID     string   `user_id`
-	UserLogin  string   `user_login`
-}
-
 // ------------- Video -------------
 
 // Video represents a video information as described by the Twitch API documentation.
@@ -204,18 +182,4 @@ type Video struct {
 type videoData struct {
 	Data       []Video `json:"data"`
 	Pagination Cursor  `json:"pagination"`
-}
-
-// VideoQueryParameters represents the query string's parameters used for API calls to the "video" endpoint.
-type VideoQueryParameters struct {
-	ID       string `id`      // required
-	UserID   string `user_id` // required
-	GameID   string `game_id` // required
-	After    string `after`
-	Before   string `before`
-	First    string `first`
-	Language string `language`
-	Period   string `period`
-	Sort     string `sort`
-	Type     string `type`
 }
