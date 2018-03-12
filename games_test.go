9f730cbf52ed21f401e174fae4600a28b876fa1e
CanobbioE <canobbio.e@gmail.com>
2018-03-12 03:44:39 +0100

    No longer ignoring test files

---
+++ games_test.go
@@ -0,0 +1,58 @@
+package twitchapi
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetGamesById(t *testing.T) {
+	var tests = []struct {
+		input GameQueryParameters
+		want  string
+	}{
+		{
+			GameQueryParameters{
+				IDs: []string{"493057"},
+			},
+			"PLAYERUNKNOWN'S BATTLEGROUNDS",
+		},
+	}
+
+	for _, test := range tests {
+		got, err := c.GetGames(test.input)
+		if err != nil {
+			s := fmt.Sprintf("%v", err)
+			t.Errorf(s)
+			return
+		}
+		if got[0].Name != test.want {
+			t.Errorf("GetGames(%v) = %s (expected %s)", test.input.IDs, got[0].Name)
+		}
+	}
+}
+
+func TestGetGamesByName(t *testing.T) {
+	var tests = []struct {
+		input GameQueryParameters
+		want  string
+	}{
+		{
+			GameQueryParameters{
+				Names: []string{"PLAYERUNKNOWN'S BATTLEGROUNDS"},
+			},
+			"493057",
+		},
+	}
+
+	for _, test := range tests {
+		got, err := c.GetGames(test.input)
+		if err != nil {
+			s := fmt.Sprintf("GetGames(%v) = %v", test.input.Names, err)
+			t.Errorf(s)
+			return
+		}
+		if got[0].ID != test.want {
+			t.Errorf("GetGames(%v) = %s (expected %s)", test.input.Names, got[0].ID)
+		}
+	}
+}
